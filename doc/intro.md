88c7e7e7b013b9183aaa321e4f94b08c31244c82
Milosz Danczak <milosz.danczak@gmail.com>
2019-07-10 15:00:00 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ross
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ross/core_test.clj
@@ -0,0 +1,7 @@
+(ns ross.core-test
+  (:require [clojure.test :refer :all]
+            [ross.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
