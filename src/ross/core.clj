7ca022758bb8070a3e64f75882c6ed70fe7b0f51
Milosz Danczak <milosz.danczak@gmail.com>
2019-07-10 16:07:58 +0200

    WIP - use remus; normalize date key

--- src/ross/core.clj
+++ src/ross/core.clj
@@ -1,6 +1,29 @@
-(ns ross.core)
+(ns ross.core
+  (:require [remus]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-url-list [lines]
+  (clojure.string/split lines #"\n"))
+
+
+;; Some feeds are using "publishedDate", some are using "updatedDate."
+;; Reconcile this by taking either as "date".
+(defn normalize-date-key [feed-entry]
+  (dissoc (assoc feed-entry :date (or (:published-date feed-entry)
+                   (:updated-date feed-entry)))
+          :published-date
+          :updated-date))
+
+
+(defn process-url [url]
+  (let [feed (:feed (remus/parse-url url))
+        all-keys (keys (first (:entries feed)))
+        entries (map (fn [entry] (select-keys entry '(:published-date :updated-date :title :link)))
+                     (:entries feed))]
+    (clojure.pprint/pprint (map normalize-date-key entries))
+    (clojure.pprint/pprint all-keys)))
+
+
+(defn main [urls-list-filename]
+  (map process-url ; map used for side effect. Well...
+       (parse-url-list
+         (slurp urls-list-filename))))
--- test/ross/core_test.clj
+++ test/ross/core_test.clj
@@ -1,7 +1,12 @@
 (ns ross.core-test
   (:require [clojure.test :refer :all]
-            [ross.core :refer :all]))
+            [ross.core :refer [normalize-date-key]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest normalize-date-key-test
+  (testing "normalize-date-key"
+    (is (= (normalize-date-key {:published-date nil
+                                :updated-date (java.util.Date. 2019 1 1)})
+           {:date (java.util.Date. 2019 1 1)}))
+    (is (= (normalize-date-key {:published-date (java.util.Date. 2019 1 1)
+                                :updated-date nil})
+           {:date (java.util.Date. 2019 1 1)}))))
